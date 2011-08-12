4f6d46e255ce4ad58e3bd1325e28559de0b88bd7
Piers Harding <piers@catalyst.net.nz>
2011-08-13 05:24:01 +1200

    bump version

--- mkdist.sh
+++ mkdist.sh
@@ -5,7 +5,7 @@ find . -name '*.log' -type f -exec echo \> {} \;
 find . -name 'rfc*trc' -type f -exec rm -f {} \;
 find . -name '*~' -type f -exec rm -f {} \;
 
-VERS=0.31
+VERS=0.32
 DIST=sapnwrfc-$VERS
 BALL=$DIST.tar.gz
 ZIP=$DIST.zip
